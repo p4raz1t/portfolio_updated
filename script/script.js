b8271e9a7bdbc358f19c348aa97e3e4706b99aa3
Zak69 <142248106+p4raz1t@users.noreply.github.com>
2024-10-06 19:11:12 +0800

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,22 @@
+//animation for github button on top panel
+const buttons = document.getElementsByClassName('butt'),
+    forEach = Array.prototype.forEach;
+
+forEach.call(buttons, function (b) {
+    b.addEventListener('click', addElement);
+});
+
+function addElement(e) {
+    const addDiv  = document.createElement('div'),
+        mValue  = Math.max(this.clientWidth, this.clientHeight),
+        rect    = this.getBoundingClientRect();
+        sDiv    = addDiv.style,
+        px      = 'px';
+
+    sDiv.width  = sDiv.height = mValue + px;
+    sDiv.left  = e.clientX - rect.left - (mValue / 2) + px;
+    sDiv.top   = e.clientY - rect.top - (mValue / 2) + px;
+
+    addDiv.classList.add('pulse');
+    this.appendChild(addDiv);
+}
